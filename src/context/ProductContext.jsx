a938c1b470f812522e82903fd071da83ce09e658
sandhyachan <sandhyachan6@gmail.com>
2024-11-21 13:15:19 +0530

    Added json data and created productContext

--- src/context/ProductContext.jsx
+++ src/context/ProductContext.jsx
@@ -1,7 +1,20 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
-export const ProductContext = createContext()
+export const ProductContext = createContext([])
 
-export default function ProductContextProvider() {
+export default function ProductContextProvider({children}) {
+    const [data, setData] = useState([])
 
+    useEffect(()=>{
+        fetch('http://localhost:5173/products.json')
+        .then(response => response.json())
+        .then(result => setData(result))
+        .catch(err => console.log(`Error fetching data ${err}`))
+    },[])
+
+    console.log(data)
+
+    return <ProductContext.Provider value={{data}}>
+        {children}
+    </ProductContext.Provider>
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ProductContextProvider from './context/ProductContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-
+  <ProductContextProvider>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </ProductContextProvider>,
 )
